622b2b81b393addda403522ac4dde469fc322b37
Vincent Liu <liuvincent19915@gmail.com>
2017-09-14 11:34:27 -0400

    Testing req/params

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 app.get('/test/:inputStuff', function (req, res) {
-  res.send(req.params)
+  res.send(req.params.inputStuff)
 });
 
 app.listen(5000, function () {
