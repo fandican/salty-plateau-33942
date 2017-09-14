eaccd9b6516ad226b24616749409b26960cf604b
Vincent Liu <liuvincent19915@gmail.com>
2017-09-14 11:31:06 -0400

    Testing req/params

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.listen(app.get('port'), function() {
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
+app.get('/test', function (req, res) {
+  res.send('Hello Test!')
+});
 
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!')
