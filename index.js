73b182c99212407dc696750dde5866917f9ff806
Vincent Liu <liuvincent19915@gmail.com>
2017-09-14 16:33:42 -0400

    Incorporated WebStorm and tested fetch more

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ app.get('/results',function(req,res){
 	var results2 = fetch('https://api.github.com/users/github')
 		.then(function(res){
 			return res.json();
-		}).then(function(json){
-			return json;
 		})
+	/*.then(function(json){
+			return json;
+		})*/
 	res.send(results2)
 });
 
